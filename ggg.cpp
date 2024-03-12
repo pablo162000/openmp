2c0d37e1aca54ea9b1f8d0754437b3e0f004d2e3
Pablo Suntaxi <pbsun.ale@gmail.com>
2024-03-11 19:46:15 -0500

    algo

---
+++ ggg.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pbsun on 11/03/2024.
+//
+
+#include "ggg.h"
---
+++ ggg.h
@@ -0,0 +1,14 @@
+//
+// Created by pbsun on 11/03/2024.
+//
+
+#ifndef CORRECCION_EXAMEN2_GGG_H
+#define CORRECCION_EXAMEN2_GGG_H
+
+
+class ggg {
+
+};
+
+
+#endif //CORRECCION_EXAMEN2_GGG_H
